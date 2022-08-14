3d3174a7d1904085f471c440a53ba01e3466c560
Fredy Velasquez <macacoloco696@gmail.com>
2022-08-13 19:07:54 -0600

    WIP: useUsuarios component create

---
+++ src/typescript/ObjetosLiterales.tsx
@@ -0,0 +1,39 @@
+interface Persona {
+    nombreCompleto: string;
+    edad: number;
+    direccion: Direccion;
+}
+
+interface Direccion {
+    pais: string;
+    casaNo: number;
+}
+
+
+
+
+export const ObjetosLiterales = () => {
+
+    const persona: Persona = {
+        nombreCompleto: 'Fredy',
+        edad: 35,
+        direccion: {
+            pais: 'Canada',
+            casaNo: 135
+        }
+    }
+
+
+    
+
+    return (
+        <>
+            <h3>Objetos literales</h3>
+            <code>
+                <pre>
+                    {JSON.stringify(persona, null, 2)}
+                </pre>
+            </code>
+        </>
+    )
+}
